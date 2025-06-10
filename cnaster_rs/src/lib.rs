8fe3639b17bcfa5878b6d5288988f786221e1963
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-06-10 11:22:09 -0400

    fix

--- cnaster_rs/src/lib.rs
+++ cnaster_rs/src/lib.rs
@@ -1,29 +1,29 @@
 use pyo3::prelude::*;
 use std::collections::HashMap;
 
-pub struct Graph<T> {
-    pub adjacency_list: HashMap<T, Vec<T>>,
+pub struct Graph<T, W> {
+    pub adjacency_list: HashMap<T, Vec<(T, W)>>,
 }
 
-impl<T: Eq + std::hash::Hash + Clone> Graph<T> {
+impl<T: Eq + std::hash::Hash + Clone, W: Clone> Graph<T, W> {
     pub fn new() -> Self {
         Graph {
             adjacency_list: HashMap::new(),
         }
     }
 
-    pub fn add_edge(&mut self, from: T, to: T) {
+    pub fn add_edge(&mut self, from: T, to: T, weight: W) {
         self.adjacency_list
             .entry(from.clone())
             .or_insert_with(Vec::new)
-            .push(to.clone());
+            .push((to.clone(), weight.clone()));
         self.adjacency_list
             .entry(to)
             .or_insert_with(Vec::new)
-            .push(from);
+            .push((from, weight));
     }
 
-    pub fn neighbors(&self, node: &T) -> Option<&Vec<T>> {
+    pub fn neighbors(&self, node: &T) -> Option<&Vec<(T, W)>> {
         self.adjacency_list.get(node)
     }
 }
@@ -40,22 +40,22 @@ mod tests {
 
     #[test]
     fn test_graph_new() {
-        let graph: Graph<i32> = Graph::new();
+        let graph: Graph<i32, i32> = Graph::new();
         assert!(graph.adjacency_list.is_empty());
     }
 
     #[test]
     fn test_add_edge_and_neighbors() {
         let mut graph = Graph::new();
-        graph.add_edge(1, 2);
-        graph.add_edge(1, 3);
-        graph.add_edge(2, 3);
+        graph.add_edge(1, 2, 10);
+        graph.add_edge(1, 3, 20);
+        graph.add_edge(2, 3, 30);
 
         let neighbors_1 = graph.neighbors(&1).unwrap();
-        assert_eq!(neighbors_1, &vec![2, 3]);
+        assert_eq!(neighbors_1, &vec![(2, 10), (3, 20)]);
 
         let neighbors_2 = graph.neighbors(&2).unwrap();
-        assert_eq!(neighbors_2, &vec![1, 3]);
+        assert_eq!(neighbors_2, &vec![(1, 10), (3, 30)]);
 
         assert!(graph.neighbors(&4).is_none());
     }
@@ -63,12 +63,18 @@ mod tests {
     #[test]
     fn test_graph_with_strings() {
         let mut graph = Graph::new();
-        
-        graph.add_edge("A".to_string(), "B".to_string());
-        graph.add_edge("A".to_string(), "C".to_string());
+
+        graph.add_edge("A".to_string(), "B".to_string(), 1.5);
+        graph.add_edge("A".to_string(), "C".to_string(), 2.5);
 
         let neighbors = graph.neighbors(&"A".to_string()).unwrap();
-        
-        assert_eq!(neighbors, &vec!["B".to_string(), "C".to_string()]);
+
+        assert_eq!(
+            neighbors,
+            &vec![
+                ("B".to_string(), 1.5),
+                ("C".to_string(), 2.5)
+            ]
+        );
     }
 }

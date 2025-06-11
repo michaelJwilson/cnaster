1d83120f21a27dcdd91893e20e1a042a01cc08cc
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-06-11 15:38:36 -0400

    fix

--- python/cnaster/plotting.py
+++ python/cnaster/plotting.py
@@ -1,7 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_cnaster_graph(cnaster_graph, alpha=0.1):
+def plot_cnaster_graph(cnaster_graph, alpha=0.1, add_edges=True, edge_downsampling=1.):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection="3d")
 
@@ -10,17 +11,21 @@ def plot_cnaster_graph(cnaster_graph, alpha=0.1):
 
     ax.scatter(sites[:, 0], sites[:, 1], sites[:, 2], c=labels)
 
-    for from_idx, edges in cnaster_graph.adjacency_list.items():        
-        for edge in edges:
-            to_idx, weight = edge
-
-            x = [sites[from_idx, 0], sites[to_idx, 0]]
-            y = [sites[from_idx, 1], sites[to_idx, 1]]
-            z = [sites[from_idx, 2], sites[to_idx, 2]]
-
-            weight = weight if weight == 1. else weight / max_weight
+    if add_edges:
+        for from_idx, edges in cnaster_graph.adjacency_list.items():
+            if np.random.uniform() > edge_downsampling:
+                continue
             
-            ax.plot(x, y, z, color="gray", linewidth=0.5, alpha=alpha * weight)
+            for edge in edges:
+                to_idx, weight = edge
+
+                x = [sites[from_idx, 0], sites[to_idx, 0]]
+                y = [sites[from_idx, 1], sites[to_idx, 1]]
+                z = [sites[from_idx, 2], sites[to_idx, 2]]
+
+                weight = weight if weight == 1. else weight / max_weight
+            
+                ax.plot(x, y, z, color="gray", linewidth=0.5, alpha=alpha * weight)
     
     ax.set_xlabel(r"$x$")
     ax.set_ylabel(r"$y$")

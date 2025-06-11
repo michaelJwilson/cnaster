56efce2553d384cda28910ab222cd77c55df1aac
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-06-11 11:17:28 -0400

    fix

---
+++ python/cnaster/plotting.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+
+def plot_cnaster_graph(cnaster_graph):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection="3d")
+
+    
+    labels, positions = cnaster_graph.labels, cnaster_graph.positions
+    
+    ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2], c=labels)
+
+    """
+    if edges is not None:
+        for from_idx, to_idx in edges:
+            x = [sites[from_idx, 0], sites[to_idx, 0]]
+            y = [sites[from_idx, 1], sites[to_idx, 1]]
+            z = [sites[from_idx, 2], sites[to_idx, 2]]
+            
+            ax.plot(x, y, z, color="black", linewidth=0.5, alpha=0.5)
+
+            normed_edge_weights = alignment_weights / alignment_weights.max()
+
+    for (from_idx, to_idx), weight in zip(alignment_edges, normed_edge_weights):
+        x = [sites[from_idx, 0], sites[to_idx, 0]]
+        y = [sites[from_idx, 1], sites[to_idx, 1]]
+        z = [sites[from_idx, 2], sites[to_idx, 2]]
+
+        ax.plot(x, y, z, color="k", linewidth=0.5, alpha=0.25 * weight)
+    """
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$y$")
+    ax.set_zlabel(r"$z$")

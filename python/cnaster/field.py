b503a9e25c5cf7f46e0b25955378069ca03ac847
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-06-11 13:01:17 -0400

    fix

--- python/cnaster/field.py
+++ python/cnaster/field.py
@@ -3,17 +3,17 @@ import numpy as np
 
 def planar_power_law(planar_positions, k, exponent):
     modk = np.linalg.norm(k)
-
     scalars = np.dot(planar_positions, k) / modk
-    scalars /= scalars.max()
 
-    return scalars**exponent
+    return scalars **exponent
 
 
-def planar_power_law_fields(positions, max_label, vec_k, epsilon=0.25):
-    exponents = np.array([n for n in range(max_label)])
+def planar_power_law_fields(positions, max_label, vec_k):
+    exponents = np.array([n for n in range(1 + max_label)])
     planar_positions = positions[:, :-1]
 
+    # print(exponents)
+    
     fields = np.vstack(
         [planar_power_law(planar_positions, vec_k, exp) for exp in exponents]
     )

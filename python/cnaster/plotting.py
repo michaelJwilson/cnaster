7c5207d4363184ae8817433d7bab9d38ef7fe4d5
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-06-11 11:32:08 -0400

    fix

--- python/cnaster/plotting.py
+++ python/cnaster/plotting.py
@@ -5,19 +5,21 @@ def plot_cnaster_graph(cnaster_graph):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection="3d")
 
-    labels, positions = cnaster_graph.labels, cnaster_graph.positions
+    max_weight = cnaster_graph.max_edge_weight
+    labels, sites = cnaster_graph.labels, cnaster_graph.positions
 
-    ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2], c=labels)
+    ax.scatter(sites[:, 0], sites[:, 1], sites[:, 2], c=labels)
 
-    for from_idx, to_idx in edges:
-        x = [sites[from_idx, 0], sites[to_idx, 0]]
-        y = [sites[from_idx, 1], sites[to_idx, 1]]
-        z = [sites[from_idx, 2], sites[to_idx, 2]]
+    for from_idx, edges in cnaster_graph.adjacency_list.items():        
+        for edge in edges:
+            to_idx, weight = edge
 
-        ax.plot(x, y, z, color="black", linewidth=0.5, alpha=0.5)
+            x = [sites[from_idx, 0], sites[to_idx, 0]]
+            y = [sites[from_idx, 1], sites[to_idx, 1]]
+            z = [sites[from_idx, 2], sites[to_idx, 2]]
 
-        normed_edge_weights = alignment_weights / alignment_weights.max()
-
+            ax.plot(x, y, z, color="black", linewidth=0.5, alpha=0.25 * weight / max_weight)
+    
     ax.set_xlabel(r"$x$")
     ax.set_ylabel(r"$y$")
     ax.set_zlabel(r"$z$")

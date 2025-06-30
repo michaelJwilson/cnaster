13cf72ecf392ff58941efc5a4b0d072758726824
Michael J. Wilson <mw9568@crick.cs.princeton.edu>
2025-06-30 15:35:17 -0400

    fix

--- python/cnaster/omics.py
+++ python/cnaster/omics.py
@@ -6,6 +6,7 @@ from cnaster.reference import get_reference_genes
 
 logger = logging.getLogger(__name__)
 
+
 # TODO place elsewhere.
 def form_gene_snp_table(unique_snp_ids, hgtable_file, adata):
     # read gene info and keep only chr1-chr22 and genes appearing in adata
@@ -108,10 +109,10 @@ def assign_initial_fragments(
             df_gene_snp[df_gene_snp.is_interval].END.values,
         )
     )
-    
-    block_genome_intervals = [tmp_block_genome_intervals[0]]    
+
+    block_genome_intervals = [tmp_block_genome_intervals[0]]
     merged = 0
-    
+
     for x in tmp_block_genome_intervals[1:]:
         # check whether overlap with previous block
         if x[0] == block_genome_intervals[-1][0] and max(
@@ -123,17 +124,19 @@ def assign_initial_fragments(
                 max(x[2], block_genome_intervals[-1][2]),
             )
 
-            # TODO warn on excessive length; 
+            # TODO warn on excessive length;
             merged += 1
-            
+
         else:
             block_genome_intervals.append(x)
 
-    logger.info(f"Merged {100. * merged / len(tmp_block_genome_intervals):.3f}% of input ranges;")
-            
+    logger.info(
+        f"Merged {100. * merged / len(tmp_block_genome_intervals):.3f}% of input ranges;"
+    )
+
     # get block_ranges in the index of df_gene_snp
     block_ranges = []
-    
+
     for x in block_genome_intervals:
         indexes = np.where(
             (df_gene_snp.CHR.values == x[0])
@@ -146,15 +149,15 @@ def assign_initial_fragments(
         # index of rows into df_gene_snp that overlap each interval.
         # TODO can fail?
         block_ranges.append((indexes[0], indexes[-1] + 1))
-        
+
     assert np.all(
         np.array(np.array([x[1] for x in block_ranges[:-1]]))
         == np.array(np.array([x[0] for x in block_ranges[1:]]))
     )
-    
+
     # record the initial block id in df_gene_snps
     df_gene_snp["initial_block_id"] = 0
-    
+
     for i, x in enumerate(block_ranges):
         df_gene_snp.iloc[x[0] : x[1], -1] = i
 
@@ -167,33 +170,33 @@ def assign_initial_fragments(
     # TODO work through.
     while s < len(block_ranges):
         t = s
-        
+
         while t <= len(block_ranges):
             t += 1
             reach_end = t == len(block_ranges)
             change_chr = initial_block_chr[s] != initial_block_chr[t - 1]
-            
+
             # count SNP-covering UMI
             involved_snps_ids = df_gene_snp[
                 (df_gene_snp.initial_block_id >= s) & (df_gene_snp.initial_block_id < t)
             ].snp_id
-            
+
             involved_snps_ids = involved_snps_ids[~involved_snps_ids.isnull()].values
             involved_snp_idx = np.array([map_snp_index[x] for x in involved_snps_ids])
-            
+
             this_snp_umis = (
                 0
                 if len(involved_snp_idx) == 0
                 else np.sum(cell_snp_Aallele[:, involved_snp_idx])
                 + np.sum(cell_snp_Ballele[:, involved_snp_idx])
             )
-            
+
             if reach_end:
                 break
-            
+
             if change_chr:
                 t -= 1
-                
+
                 # re-count SNP-covering UMIs
                 involved_snps_ids = df_gene_snp.snp_id.iloc[
                     block_ranges[s][0] : block_ranges[t - 1][1]
@@ -201,23 +204,23 @@ def assign_initial_fragments(
                 involved_snps_ids = involved_snps_ids[
                     ~involved_snps_ids.isnull()
                 ].values
-                
+
                 involved_snp_idx = np.array(
                     [map_snp_index[x] for x in involved_snps_ids]
                 )
-                
+
                 this_snp_umis = (
                     0
                     if len(involved_snp_idx) == 0
                     else np.sum(cell_snp_Aallele[:, involved_snp_idx])
                     + np.sum(cell_snp_Ballele[:, involved_snp_idx])
                 )
-                
+
                 break
-            
+
             if this_snp_umis >= initial_min_umi:
                 break
-        
+
         if (
             this_snp_umis < initial_min_umi
             and s > 0
@@ -228,15 +231,120 @@ def assign_initial_fragments(
         else:
             indexes = np.where(df_gene_snp.initial_block_id.isin(np.arange(s, t)))[0]
             block_ranges_new.append((indexes[0], indexes[-1] + 1))
-            
+
         s = t
 
     # record the block id in df_gene_snps
     df_gene_snp["block_id"] = 0
-    
+
     for i, x in enumerate(block_ranges_new):
         df_gene_snp.iloc[x[0] : x[1], -1] = i
 
     df_gene_snp = df_gene_snp.drop(columns=["initial_block_id"])
-    
+
     return df_gene_snp
+
+
+def summarize_counts_for_blocks(
+    df_gene_snp,
+    adata,
+    cell_snp_Aallele,
+    cell_snp_Ballele,
+    unique_snp_ids,
+    nu,
+    logphase_shift,
+    geneticmap_file,
+):
+    """
+    Attributes:
+    ----------
+    df_gene_snp : pd.DataFrame
+        Contain "block_id" column to indicate which genes/snps belong to which block.
+
+    Returns
+    ----------
+    lengths : array, (n_chromosomes,)
+        Number of blocks per chromosome.
+
+    single_X : array, (n_blocks, 2, n_spots)
+        Transcript counts and B allele count per block per cell.
+
+    single_base_nb_mean : array, (n_blocks, n_spots)
+        Baseline transcript counts in normal diploid per block per cell.
+
+    single_total_bb_RD : array, (n_blocks, n_spots)
+        Total allele count per block per cell.
+
+    log_sitewise_transmat : array, (n_blocks,)
+        Log phase switch probability between each pair of adjacent blocks.
+    """
+    blocks = df_gene_snp.block_id.unique()
+    single_X = np.zeros((len(blocks), 2, adata.shape[0]), dtype=int)
+    single_base_nb_mean = np.zeros((len(blocks), adata.shape[0]))
+    single_total_bb_RD = np.zeros((len(blocks), adata.shape[0]), dtype=int)
+    # summarize counts of involved genes and SNPs within each block
+    map_snp_index = {x: i for i, x in enumerate(unique_snp_ids)}
+    df_block_contents = df_gene_snp.groupby("block_id").agg(
+        {"snp_id": list, "gene": list}
+    )
+    for b in range(df_block_contents.shape[0]):
+        # BAF (SNPs)
+        involved_snps_ids = [
+            x for x in df_block_contents.snp_id.values[b] if not x is None
+        ]
+        involved_snp_idx = np.array([map_snp_index[x] for x in involved_snps_ids])
+        if len(involved_snp_idx) > 0:
+            single_X[b, 1, :] = np.sum(cell_snp_Aallele[:, involved_snp_idx], axis=1)
+            single_total_bb_RD[b, :] = np.sum(
+                cell_snp_Aallele[:, involved_snp_idx], axis=1
+            ) + np.sum(cell_snp_Ballele[:, involved_snp_idx], axis=1)
+        # RDR (genes)
+        involved_genes = list(
+            set([x for x in df_block_contents.gene.values[b] if not x is None])
+        )
+        if len(involved_genes) > 0:
+            single_X[b, 0, :] = np.sum(
+                adata.layers["count"][:, adata.var.index.isin(involved_genes)], axis=1
+            )
+
+    # lengths
+    lengths = np.zeros(len(df_gene_snp.CHR.unique()), dtype=int)
+    for i, c in enumerate(df_gene_snp.CHR.unique()):
+        lengths[i] = len(df_gene_snp[df_gene_snp.CHR == c].block_id.unique())
+
+    # phase switch probability from genetic distance
+    sorted_chr_pos_first = df_gene_snp.groupby("block_id").agg(
+        {"CHR": "first", "START": "first"}
+    )
+    sorted_chr_pos_first = list(
+        zip(sorted_chr_pos_first.CHR.values, sorted_chr_pos_first.START.values)
+    )
+    sorted_chr_pos_last = df_gene_snp.groupby("block_id").agg(
+        {"CHR": "last", "END": "last"}
+    )
+    sorted_chr_pos_last = list(
+        zip(sorted_chr_pos_last.CHR.values, sorted_chr_pos_last.END.values)
+    )
+    #
+    tmp_sorted_chr_pos = [
+        val for pair in zip(sorted_chr_pos_first, sorted_chr_pos_last) for val in pair
+    ]
+    position_cM = get_position_cM_table(tmp_sorted_chr_pos, geneticmap_file)
+    phase_switch_prob = compute_phase_switch_probability_position(
+        position_cM, tmp_sorted_chr_pos, nu
+    )
+    log_sitewise_transmat = np.minimum(
+        np.log(0.5), np.log(phase_switch_prob) - logphase_shift
+    )
+    # log_sitewise_transmat = log_sitewise_transmat[np.arange(0, len(log_sitewise_transmat), 2)]
+    log_sitewise_transmat = log_sitewise_transmat[
+        np.arange(1, len(log_sitewise_transmat), 2)
+    ]
+
+    return (
+        lengths,
+        single_X,
+        single_base_nb_mean,
+        single_total_bb_RD,
+        log_sitewise_transmat,
+    )

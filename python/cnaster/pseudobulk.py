512d9445272e872217cefef471e0776ee66eed60
Michael J. Wilson <mw9568@rinse.cs.princeton.edu>
2025-07-01 15:47:28 -0400

    fix

---
+++ python/cnaster/pseudobulk.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def merge_pseudobulk_by_index(
+    single_X,
+    single_base_nb_mean,
+    single_total_bb_RD,
+    clone_index,
+    single_tumor_prop,
+    threshold=0.5,
+):
+    n_obs = single_X.shape[0]
+    n_spots = len(clone_index)
+    
+    X = np.zeros((n_obs, 2, n_spots))
+    
+    base_nb_mean = np.zeros((n_obs, n_spots))
+    total_bb_RD = np.zeros((n_obs, n_spots))
+    tumor_prop = np.zeros(n_spots)
+
+    for k, idx in enumerate(clone_index):
+        if len(idx) == 0:
+            continue
+        
+        idx = idx[np.where(single_tumor_prop[idx] > threshold)[0]]
+
+        # TODO assumes simple aggregation.
+        X[:, :, k] = np.sum(single_X[:, :, idx], axis=2)
+        
+        base_nb_mean[:, k] = np.sum(single_base_nb_mean[:, idx], axis=1)
+        total_bb_RD[:, k] = np.sum(single_total_bb_RD[:, idx], axis=1)
+        
+        tumor_prop[k] = np.mean(single_tumor_prop[idx]) if len(idx) > 0 else 0
+
+    return X, base_nb_mean, total_bb_RD, tumor_prop

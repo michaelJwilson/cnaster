77084fe92f2b72dc65b63b3af8c5a87ad8d5d876
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-07-02 12:44:26 -0400

    fix

--- python/cnaster/scripts/prep_snps.py
+++ python/cnaster/scripts/prep_snps.py
@@ -6,8 +6,8 @@ import numpy as np
 import pandas as pd
 
 
-def main(cellsnplite_result_dir, eagle_out_dir, vaf_threshold=0.1):
-    cellsnp_base = [str(x) for x in Path(cellsnplite_result_dir).glob("cellSNP.base*")][
+def main(cellsnplite_results_dir, eagle_results_dir, vaf_threshold=0.1):
+    cellsnp_base = [str(x) for x in Path(cellsnplite_results_dir).glob("cellSNP.base*")][
         0
     ]
     
@@ -19,10 +19,11 @@ def main(cellsnplite_result_dir, eagle_out_dir, vaf_threshold=0.1):
     )
     
     df_snp["CHROM"] = [f"chr{x}" for x in df_snp.tmpCHR]
+    
     df_snp["AD"] = [int(x.split(";")[0].split("=")[-1]) for x in df_snp.INFO]
     df_snp["DP"] = [int(x.split(";")[1].split("=")[-1]) for x in df_snp.INFO]
 
-    # NB OTH: Count of "other" alleles not classified as the main alternative
+    # NB OTH: count of "other" alleles not classified as the main alternative
     df_snp["OTH"] = [int(x.split(";")[2].split("=")[-1]) for x in df_snp.INFO]
     
     # remove records with DP == 0;
@@ -97,7 +98,7 @@ def main(cellsnplite_result_dir, eagle_out_dir, vaf_threshold=0.1):
         
         df.sort_values(by="POS", inplace=True)
         
-        fp = open(f"{eagle_out_dir}/chr{c}.vcf", "w")
+        fp = open(f"{eagle_results_dir}/chr{c}.vcf", "w")
         fp.write("##fileformat=VCFv4.2\n")
         fp.write(
             '##FORMAT=<ID=GT,Number=1,Type=String,Description="Consensus Genotype across all datasets with called genotype">\n'
@@ -112,10 +113,10 @@ def main(cellsnplite_result_dir, eagle_out_dir, vaf_threshold=0.1):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c", "--cellsnplite_result_dir", help="cellsnplite result directory", type=str
+        "-c", "--cellsnplite_results_dir", help="cellsnplite result directory", type=str
     )
     parser.add_argument(
-        "-o", "--eagle_out_dir", help="eagle output directory", type=str
+        "-o", "--eagle_results_dir", help="eagle output directory", type=str
     )
     parser.add_argument(
         "-v", "--vaf_threshold", help="vaf threshold", default=0.1, type=float
@@ -124,5 +125,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     main(
-        args.cellsnplite_result_dir, args.eagle_out_dir, args.vaf_threshold
+        args.cellsnplite_results_dir, args.eagle_results_dir, args.vaf_threshold
     )

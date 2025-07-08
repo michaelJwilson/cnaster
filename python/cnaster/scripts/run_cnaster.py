c0fcd80a169deb03406dc0aac24d2c57cf66f346
Michael J. Wilson <mw9568@rinse.cs.princeton.edu>
2025-07-08 13:54:48 -0400

    fix

--- python/cnaster/scripts/run_cnaster.py
+++ python/cnaster/scripts/run_cnaster.py
@@ -9,7 +9,7 @@ from cnaster.omics import (
     summarize_counts_for_bins,
     summarize_counts_for_blocks,
     create_bin_ranges,
-    summarize_counts_for_bins
+    summarize_counts_for_bins,
 )
 from cnaster.phasing import initial_phase_given_partition
 from cnaster.spatial import initialize_clones, multislice_adjacency
@@ -28,7 +28,6 @@ def main():
     config_path = f"/u/mw9568/research/repos/cnaster/config_turing.yaml"
     config = YAMLConfig.from_file(config_path)
 
-    """
     # RUN
     (
         adata,
@@ -37,41 +36,45 @@ def main():
         unique_snp_ids,
         across_slice_adjacency_mat,
     ) = load_input_data(
-        config
+        config,
         filter_gene_file=config.references.filtergenelist_file,
         filter_range_file=config.references.filterregion_file,
     )
 
     # TODO CHECK
     coords = adata.obsm["X_pos"]
-    
+
     sample_list = [adata.obs["sample"][0]]
-    
+
     for i in range(1, adata.shape[0]):
         if adata.obs["sample"][i] != sample_list[-1]:
-            sample_list.append( adata.obs["sample"][i] )
-            
+            sample_list.append(adata.obs["sample"][i])
+
     # convert sample name to index
     sample_ids = np.zeros(adata.shape[0], dtype=int)
-    
-    for s,sname in enumerate(sample_list):
+
+    for s, sname in enumerate(sample_list):
         index = np.where(adata.obs["sample"] == sname)[0]
         sample_ids[index] = s
 
     # TODO
     if not config.preprocessing.tumorprop_file is None:
-        df_tumorprop = pd.read_csv(config["tumorprop_file"], sep="\t", header=0, index_col=0)
+        df_tumorprop = pd.read_csv(
+            config["tumorprop_file"], sep="\t", header=0, index_col=0
+        )
         df_tumorprop = df_tumorprop[["Tumor"]]
         df_tumorprop.columns = ["tumor_proportion"]
-        
+
         adata.obs = adata.obs.join(df_tumorprop)
-        
+
         single_tumor_prop = adata.obs["tumor_proportion"]
     else:
         single_tumor_prop = None
-        
+
     # RUN
-    df_gene_snp = form_gene_snp_table(unique_snp_ids, config.references.hgtable_file, adata)
+    df_gene_snp = form_gene_snp_table(
+        unique_snp_ids, config.references.hgtable_file, adata
+    )
 
     # RUN
     df_gene_snp = assign_initial_blocks(
@@ -112,31 +115,31 @@ def main():
         single_total_bb_RD,
         single_tumor_prop,
         initial_clone_for_phasing,
-        5, # MAGIC n_states
+        5,  # MAGIC n_states
         log_sitewise_transmat,
-        "sp", # MAGIC params (start prob. and baf states).
+        "sp",  # MAGIC params (start prob. and baf states).
         config.hmm.t_phaseing,
         config.hmm.gmm_random_state,
         config.hmm.fix_NB_dispersion,
         config.hmm.shared_NB_dispersion,
         config.hmm.fix_BB_dispersion,
         config.hmm.shared_BB_dispersion,
-        30, # MAGIC max_iter
-        1.e-3, # MAGIC tol
+        30,  # MAGIC max_iter
+        1.0e-3,  # MAGIC tol
         threshold=config.hmrf.tumorprop_threshold,
     )
 
-    # RUN 
+    # RUN
     df_gene_snp["phase"] = np.where(
         df_gene_snp.snp_id.isnull(),
         None,
         df_gene_snp.block_id.map({i: x for i, x in enumerate(phase_indicator)}),
     )
-    
+
     df_gene_snp = create_bin_ranges(
         df_gene_snp, single_total_bb_RD, refined_lengths, config["secondary_min_umi"]
     )
-    
+
     (
         lengths,
         single_X,
@@ -153,16 +156,27 @@ def main():
         logphase_shift=config["logphase_shift"],
         geneticmap_file=config["geneticmap_file"],
     )
-    
-    # expression count dataframe
-    exp_counts = pd.DataFrame.sparse.from_spmatrix( scipy.sparse.csc_matrix(adata.layers["count"]), index=adata.obs.index, columns=adata.var.index)
-    
-    # smooth and adjacency matrix for each sample
-    adjacency_mat, smooth_mat = multislice_adjacency(sample_ids, sample_list, coords, single_total_bb_RD, exp_counts, 
-                                                     across_slice_adjacency_mat, construct_adjacency_method=config['construct_adjacency_method'], 
-                                                     maxspots_pooling=config['maxspots_pooling'], construct_adjacency_w=config['construct_adjacency_w'])
+
+    # NB expression count dataframe
+    exp_counts = pd.DataFrame.sparse.from_spmatrix(
+        scipy.sparse.csc_matrix(adata.layers["count"]),
+        index=adata.obs.index,
+        columns=adata.var.index,
+    )
+
+    # NB smooth & adjacency matrix for each sample
+    adjacency_mat, smooth_mat = multislice_adjacency(
+        sample_ids,
+        sample_list,
+        coords,
+        single_total_bb_RD,
+        exp_counts,
+        across_slice_adjacency_mat,
+        construct_adjacency_method=config["construct_adjacency_method"],
+        maxspots_pooling=config["maxspots_pooling"],
+        construct_adjacency_w=config["construct_adjacency_w"],
+    )
 
     n_pooled = np.median(np.sum(smooth_mat > 0, axis=0).A.flatten())
-    """
 
     logger.info("Done.\n\n")

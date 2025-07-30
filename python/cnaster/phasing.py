33b150992819e38d76d89f7eb40a2714fe82c0cd
Michael J. Wilson <mw9568@turing.cs.princeton.edu>
2025-07-30 15:50:27 -0400

    fix.

--- python/cnaster/phasing.py
+++ python/cnaster/phasing.py
@@ -152,6 +152,6 @@ def initial_phase_given_partition(
 
     refined_lengths = np.array(refined_lengths)
 
-    logger.info(f"Solved for {len(refined_lengths)} phase-refined lengths given {len(length)} input lengths.")
+    logger.info(f"Solved for {len(refined_lengths)} phase-refined lengths given {len(lengths)} input lengths.")
     
     return phase_indicator, refined_lengths

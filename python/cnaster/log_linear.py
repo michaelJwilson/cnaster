20ac55cde32922affc1367e51bedb9644578d51e
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-10-27 14:34:38 -0400

    fix

--- python/cnaster/log_linear.py
+++ python/cnaster/log_linear.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import transforms as mtransforms, scale as mscale, ticker as mticker
-
+import matplotlib.ticker as mticker
 
 class LinearThenLogTransform(mtransforms.Transform):
     input_dims = output_dims = 1
@@ -48,6 +48,33 @@ class InvertedLinearThenLogTransform(mtransforms.Transform):
     def inverted(self):
         return LinearThenLogTransform(self.threshold, self.base)
 
+class LinearThenLogMinorLocator(mticker.Locator):
+    """Minor locator: auto below threshold, log-like above threshold."""
+    def __init__(self, threshold=1.0, base=10.0):
+        self.threshold = threshold
+        self.base = base
+
+    def __call__(self):
+        vmin, vmax = self.axis.get_view_interval()
+        if vmax <= self.threshold:
+            # entirely linear region -> no minor ticks (or use AutoMinorLocator)
+            return []
+        # above threshold: use log minor ticks
+        # map back to data space
+        transform = self.axis.get_transform()
+        inv = transform.inverted()
+        # get log-spaced minors in transformed space
+        t_min = max(self.threshold, vmin)
+        t_max = vmax
+        # simple approach: log minors from threshold to vmax
+        log_min = np.log(t_min / self.threshold) / np.log(self.base)
+        log_max = np.log(t_max / self.threshold) / np.log(self.base)
+        decades = np.arange(np.floor(log_min), np.ceil(log_max) + 1)
+        ticks = self.threshold * (self.base ** decades)
+        # filter to view range
+        ticks = ticks[(ticks >= vmin) & (ticks <= vmax)]
+        return ticks
+
 
 class LinearThenLogScale(mscale.ScaleBase):
     name = "linlog"
@@ -62,7 +89,7 @@ class LinearThenLogScale(mscale.ScaleBase):
 
     def set_default_locators_and_formatters(self, axis):
         axis.set_major_locator(mticker.AutoLocator())
-        axis.set_minor_locator(mticker.AutoMinorLocator())
+        axis.set_minor_locator(LinearThenLogMinorLocator(self.threshold, self.base))
         axis.set_major_formatter(mticker.ScalarFormatter())
 
     def limit_range_for_scale(self, vmin, vmax, minpos):

818cd44763a1ae5fa76a5352b06a50c16159df44
Michael J. Wilson <mw9568@crick.cs.princeton.edu>
2025-11-05 13:42:51 -0500

    fix

---
+++ batch_zenodo_sim_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SAMPLE_IDS=("numcnas1.2_cnasize1e7_ploidy2_random0")
+
+for SAMPLE_ID in "${SAMPLE_IDS[@]}"; do
+    # NB replace numcnas1.2_cnasize1e7_ploidy2_random0 in ./zenodo_sample_sheet.tsv with {SAMPLE_ID} and write to zenodo_sample_sheets/zenodo_{SAMPLE_ID}_sheet.tsv
+    sed "s|numcnas1.2_cnasize1e7_ploidy2_random0|${SAMPLE_ID}|g" ./zenodo_sample_sheet.tsv \
+        > "zenodo_sample_sheets/zenodo_${SAMPLE_ID}_sheet.tsv"
+    
+    run_cnaster zenodo_sim_config \
+        -o "paths.sample_sheet=zenodo_sample_sheets/zenodo_${SAMPLE_ID}_sheet.tsv" \
+        -o "paths.output_dir=/u/mw9568/scratch/calicost_sims/nomixing_cnaster_related/${SAMPLE_ID}/" \
+        -o "annotation.clone_label=/u/mw9568/scratch/calicost_sims/simulated_data_related/${SAMPLE_ID}/truth_clone_labels.tsv"
+done

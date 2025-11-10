eb0d23fa29a7bfa8ab5b3fc38460fe41e870a0c4
Michael J. Wilson <mw9568@crick.cs.princeton.edu>
2025-11-10 13:02:31 -0500

    fix

--- analysis.py
+++ analysis.py
@@ -1,28 +1,50 @@
 import logging
+import numpy as np
 import pandas as pd
 import pyranges as pr
 
 logger = logging.getLogger(__name__)
 
-def remap_clone_num(columns):
-    new_columns = {}
 
-    for col in columns:
-        if col.startswith("normal_"):
-            # normal -> true_clone_0
-            new_col = col.replace("normal_", "0_").replace("_copy", "")
-            new_columns[col] = new_col
-        elif col.startswith("clone_"):
-            # Extract clone number and increment by 1
-            parts = col.split("_")
-            clone_num = int(parts[1]) + 1
-            allele = parts[2]  # A or B
-            new_columns[col] = f"tru_clone_{clone_num}_{allele}"
+def remap_clone_num(entries):
+    entries = np.unique(entries)
 
-    return new_columns
+    # logger.debug(f"Found unique entries: {entries}")
+    
+    has_normal = np.any(["normal" in xx for xx in entries])
+    clone_zp = 1 if has_normal else 0
+
+    new_entries = {}
+
+    for entry in entries:
+        orig_entry = entry
+        entry = entry.replace("_copy", "")
+
+        if entry in new_entries:
+            continue
+
+        if entry.startswith("normal"):
+            new_entries[orig_entry] = entry.replace("normal", "clone_0")
+
+        elif entry.startswith("clone"):
+            parts = entry.split("_")
+
+            clone_num = int(parts[1])
+            new_clone_num = clone_num + clone_zp
+
+            new_entries[orig_entry] = entry.replace(
+                f"clone_{clone_num}", f"clone_{new_clone_num}"
+            )
+
+    # logger.debug(
+    #     f"Found entry mapper={new_entries} with has_normal={has_normal} and clone_zp={clone_zp}"
+    # )
+
+    return new_entries
+
 
 def get_sample_truth(root, sample_id, fname="truth_acn_profile.tsv"):
-    # NB 
+    # NB
     #         labels  x       y
     # spot_0  clone_2 0       0
     truth_clones = pd.read_csv(
@@ -32,19 +54,32 @@ def get_sample_truth(root, sample_id, fname="truth_acn_profile.tsv"):
         skiprows=1,
     )
 
-    truth_clones["true_clone"] = truth_clones["true_clone"].str.replace("clone_", "").astype(int)
+    truth_clones["true_clone"] = truth_clones["true_clone"].map(
+        remap_clone_num(truth_clones["true_clone"])
+    )
 
     spots = truth_clones["barcode"].unique()
     spot_to_clone = dict(zip(truth_clones["barcode"], truth_clones["true_clone"]))
 
-    logger.info(f"Found {truth_clones['true_clone'].unique()} clones in truth for sample {sample_id} spaceranger.")
+    # logger.debug(
+    #     f"Found {truth_clones['true_clone'].unique()} clones in truth for sample {sample_id} spaceranger."
+    # )
+    # logger.info(f"Found truth clones:\n{truth_clones}")
+
     # NB
     # clone	    chr	    start	    end	        A_copy	B_copy
     # clone_0	20	    51816053	61816053	0	    1
     truth = pd.read_csv(
         f"{root}/simulated_data_related/{sample_id}/{fname}",
         sep="\t",
-    ).rename(columns={"chr": "Chromosome", "start": "Start", "end": "End"})
+    ).rename(
+        columns={
+            "chr": "Chromosome",
+            "start": "Start",
+            "end": "End",
+            "clone": "true_clone",
+        }
+    )
 
     copy_num_columns = truth.columns[3:]
 
@@ -54,11 +89,9 @@ def get_sample_truth(root, sample_id, fname="truth_acn_profile.tsv"):
     # NB remap normal to clone 0 and increment by 1 otherwise.
     truth_cna = truth_cna.rename(columns=remap_clone_num(copy_num_columns))
 
-    logger.info(f"Found {truth_cna['true_clone'].unique()} clones in truth for sample {sample_id} cna.")
+    # logger.info(f"Found truth cna:\n{truth_cna}")
 
-    assert truth_clones['label'].unique() == truth_cna['label'].unique(), "Mismatch between clone labels in truth clones and truth cna: {truth_clones['label'].unique()} != {truth_cna['label'].unique()}"
-
-    # NB expand to per-spot CNA truth ... 
+    # NB expand to per-spot CNA truth ...
     expanded_rows = []
 
     for _, seg in truth_cna.iterrows():
@@ -73,8 +106,8 @@ def get_sample_truth(root, sample_id, fname="truth_acn_profile.tsv"):
                     "End": seg["End"],
                     "barcode": spot,
                     "true_clone": clone_num,
-                    "true_A": seg.get(f"true_clone_{clone_num}_A"),
-                    "true_B": seg.get(f"true_clone_{clone_num}_B"),
+                    "true_A": seg.get(f"clone_{clone_num}_A"),
+                    "true_B": seg.get(f"clone_{clone_num}_B"),
                 }
             )
 
@@ -82,24 +115,29 @@ def get_sample_truth(root, sample_id, fname="truth_acn_profile.tsv"):
     spot_truth_cna = pr.PyRanges(spot_truth_cna)
     spot_truth_cna.insert(3, "sample_id", sample_id)
 
+    logger.info(f"Found true CNAs:\n{spot_truth_cna}")
+    
     return spot_truth_cna
 
+
 def get_sample_calicost(root, sample_id):
-    # NB 
+    # NB
     # barcode sample_id       x       y       clone_label
     # spot_0  0       0       0       3
     calicost_clones = pd.read_csv(
         f"{root}/nomixing_calicost_related/{sample_id}/clone_labels.tsv",
         sep="\t",
         usecols=["barcode", "x", "y", "clone_label"],
-    ).rename(columns={"clone_label": "label"})
+    ).rename(columns={"clone_label": "clone"})
 
     spots = calicost_clones["barcode"].unique()
     spot_to_calicost_clone = dict(
-        zip(calicost_clones["barcode"], calicost_clones["label"])
+        zip(calicost_clones["barcode"], calicost_clones["clone"])
     )
 
-    logger.info(f"Found {calicost_clones['label'].unique()} clones in CalicoST for sample {sample_id}.")
+    # logger.info(
+    # f"Found {calicost_clones['clone'].unique()} clones in CalicoST for sample {sample_id}."
+    # )
 
     calicost = pd.read_csv(
         f"{root}/nomixing_calicost_related/{sample_id}/cnv_seglevel.tsv",
@@ -115,8 +153,6 @@ def get_sample_calicost(root, sample_id):
     calicost_cna.columns = calicost_cna.columns.str.replace(
         r"clone(\d+)\s+([AB])", r"clone_\1_\2", regex=True
     )
-
-    assert calicost_clones['label'].unique() == calicost_cna['label'].unique(), "Mismatch between clone labels in calicost clones and calicost cna: {calicost_clones['label'].unique()} != {calicost_cna['label'].unique()}"
 
     # NB truth per spot, per segment ...
     calicost_expanded_rows = []
@@ -140,10 +176,15 @@ def get_sample_calicost(root, sample_id):
     spot_calicost_cna = pr.PyRanges(spot_calicost_cna)
     spot_calicost_cna.insert(3, "sample_id", sample_id)
 
+    logger.info(f"Found calicost CNAs:\n{spot_calicost_cna}")
+    
     return spot_calicost_cna
 
+
 def get_truth_calicost_join(spot_truth_cna, spot_calicost_cna):
-    spot_join_cna = spot_truth_cna.join_overlaps(spot_calicost_cna, match_by=["barcode", "sample_id"])
+    spot_join_cna = spot_truth_cna.join_overlaps(
+        spot_calicost_cna, match_by=["barcode", "sample_id"]
+    )
 
     start_b = spot_join_cna.pop("Start_b")
     end_b = spot_join_cna.pop("End_b")
@@ -159,12 +200,8 @@ if __name__ == "__main__":
     sample_id = "numcnas1.2_cnasize1e7_ploidy2_random0"
 
     spot_truth_cna = get_sample_truth(root, sample_id)
-
-    exit(0)
-
-    
     spot_calicost_cna = get_sample_calicost(root, sample_id)
-
+    
     spot_join_cna = get_truth_calicost_join(spot_truth_cna, spot_calicost_cna)
 
     success_rate = (
@@ -174,9 +211,9 @@ if __name__ == "__main__":
 
     print(success_rate)
 
-    spot_join_cna = spot_join_cna[~(spot_join_cna[["true_A", "true_B"]].eq(1).all(axis=1))]
-
-    print(spot_join_cna)
+    spot_join_cna = spot_join_cna[
+        ~(spot_join_cna[["true_A", "true_B"]].eq(1).all(axis=1))
+    ]
 
     success_rate = (
         (spot_join_cna["A"] == spot_join_cna["true_A"])

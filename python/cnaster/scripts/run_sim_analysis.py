6f27e9c6679e4e1cdf949a1ffdd3021d8ca3ab30
Michael J. Wilson <mw9568@crick.cs.princeton.edu>
2025-11-14 11:23:32 -0500

    fix

--- python/cnaster/scripts/run_sim_analysis.py
+++ python/cnaster/scripts/run_sim_analysis.py
@@ -668,7 +668,7 @@ def main():
     # "numcnas3.3_cnasize5e7_ploidy2_random0",
 
     sample_ids = [
-        "numcnas3.3_cnasize3e7_ploidy2_random0",
+        "numcnas1.2_cnasize1e7_ploidy2_random0",
     ]
 
     logger.info(

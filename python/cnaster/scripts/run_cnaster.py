ba61cec5af48f218d6c8b1e1589f875d7776ae01
Michael J. Wilson <mw9568@crick.cs.princeton.edu>
2025-12-03 12:12:07 -0500

    fix

--- python/cnaster/scripts/run_cnaster.py
+++ python/cnaster/scripts/run_cnaster.py
@@ -545,8 +545,6 @@ def run_cnaster(config_path, over_rides=None):
 
     fig_path = f"{plots_dir}/initial_clones_spatial.pdf"
     write_fig(fig_path, initial_clones_fig, transparent=True, bbox_inches="tight")
-
-    exit(0)
     
     logger.info(
         "Solving HMM & HMRF for copy states and clone assignment with BAF only."

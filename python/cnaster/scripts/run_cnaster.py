bda21a4a9be53f6b140b4c08d1ca7111c5492870
Michael J. Wilson <michaeljwilson@princeton.edu>
2025-12-04 09:47:38 -0500

    fix

--- python/cnaster/scripts/run_cnaster.py
+++ python/cnaster/scripts/run_cnaster.py
@@ -683,8 +683,6 @@ def run_cnaster(config_path, over_rides=None):
         fig_path, merged_bafonly_clones_fig, transparent=True, bbox_inches="tight"
     )
 
-    exit(0)
-
     # NB construct clone labels.
     df_clone_label = pd.DataFrame(
         {"x": coords[:, 0], "y": coords[:, 1]}, index=barcodes

11492c85b618a3b59d2f22fa7717b214e303e7a5
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-01-26 13:04:25 -0500

    fix

--- python/cnaster/phasing.py
+++ python/cnaster/phasing.py
@@ -207,7 +207,7 @@ def initial_phase_given_partition(
                 >= BAF_CHANGE_THRESHOLD
             ):
                 # NB new blocks are a min. size and set by change in BAF.
-                logger.warning(f"Forced a block boundary at contig {1 + ii} pos {i} given dBAF={np.abs(minor_baf_profiles[:, i + cumlen] - minor_baf_profiles[:, i + cumlen - 1]).max()}.")
+                logger.warning(f"Forced a block boundary at contig {1 + ii} pos {i} given dBAF={np.abs(minor_baf_profiles[:, i + cumlen] - minor_baf_profiles[:, i + cumlen - 1]).max():.4f}.")
                 refined_lengths.append(i - s)
                 s = i
 

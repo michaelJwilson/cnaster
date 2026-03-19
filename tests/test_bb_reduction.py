a5e3ad2349e0208c0fd2a40e6fd3fb953f39d956
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-03-19 11:28:56 -0400

    fix

--- tests/test_bb_reduction.py
+++ tests/test_bb_reduction.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.stats import betabinom
 from scipy.optimize import minimize
 from collections import namedtuple
+from numba import njit
 
 beta_binomial_params = namedtuple("beta_binomial_params", ["alpha", "beta"])
 
@@ -57,58 +58,105 @@ def test_mle(mock_bb_dataset, benchmark):
     np.testing.assert_allclose(result.x[1], exp_beta, rtol=1e-1)
 
 
+@njit
 def get_model_vectors(kmax, dkmax, nmax, alpha, beta):
     tau = alpha + beta
     base = np.arange(nmax)
 
-    A = np.log(alpha + base[kmax - 1])
-    B = np.log(beta + base[dkmax - 1])
-    N = np.log(tau + base[nmax - 1])
+    # NB len(A) = kmax; len(B) = dkmax; len(N) = nmax; i.e. 0 to kmax-1, etc.
+    A = np.log(alpha + base[:kmax])
+    B = np.log(beta + base[:dkmax])
+    N = np.log(tau + base[:nmax])
 
     return A, B, N
 
 
-def get_window_matrix(wi, ci, ci_max):
-    # NB each row is sum_i wi for ci = i;
-    W = np.zeros((ci_max, ci_max))
-    for i in range(ci_max):
-        mask = ci == i
-        result = np.sum(wi[mask])
+def test_get_model_vectors(mock_bb_dataset):
+    (ki, ni), (alpha, beta) = mock_bb_dataset
 
+    kmax = int(np.max(ki))
+    dkmax = int(np.max(ni - ki))
+    nmax = int(np.max(ni))
+
+    A, B, N = get_model_vectors(kmax, dkmax, nmax, alpha, beta)
+
+    assert len(A) == kmax
+    assert len(B) == dkmax
+    assert len(N) == nmax
+
+
+@njit
+def get_window_matrix(wi, ci):
+    # NB window matrix Wij = sum_i wi * 1(ci == i) for j < i; otherwise 0.
+    #    W.shape = (1 + ci_max, ci_max), i.e. i = 0 ... ci_max, j = 0 ... ci_max-1;
+    ci_max = int(np.max(ci))
+
+    totals = np.zeros(1 + ci_max)
+    for k in range(len(ci)):
+        idx = ci[k]
+        totals[idx] += wi[k]
+
+    W = np.zeros((1 + ci_max, ci_max))
+    for i in range(1 + ci_max):
         for j in range(i):
-            W[i, j] = result
+            W[i, j] = totals[i]
 
     return W
 
 
+def test_get_window_matrix(mock_bb_dataset):
+    (ki, _), _ = mock_bb_dataset
+    wi = np.ones_like(ki, dtype=float)
+
+    kmax = int(np.max(ki))
+    W = get_window_matrix(wi, ki.astype(np.int64))
+
+    assert W.shape == (1 + kmax, kmax)
+
+
+def get_window_matrices(wi, ki, ni):
+    Aw = get_window_matrix(wi, ki.astype(np.int64))
+    Bw = get_window_matrix(wi, (ni - ki).astype(np.int64))
+    Nw = get_window_matrix(wi, ni.astype(np.int64))
+
+    return Aw, Bw, Nw
+
+
+@njit
 def weighted_nll_fast(params, Aw, Bw, Nw):
     alpha, beta = params
 
-    kmax = Aw.shape[0]
-    dkmax = Bw.shape[0]
-    nmax = Nw.shape[0]
+    amax = Aw.shape[1]
+    bmax = Bw.shape[1]
+    nmax = Nw.shape[1]
 
-    A, B, N = get_model_vectors(kmax, dkmax, nmax, alpha, beta)
+    A, B, N = get_model_vectors(amax, bmax, nmax, alpha, beta)
 
-    return -(Aw @ A + Bw @ B - Nw @ N)
+    return -(np.sum(Aw @ A) + np.sum(Bw @ B) - np.sum(Nw @ N))
+
+
+def test_weighted_nll_fast(mock_bb_dataset):
+    (ki, ni), (alpha, beta) = mock_bb_dataset
+    wi = np.ones_like(ki, dtype=float)
+
+    Aw, Bw, Nw = get_window_matrices(wi, ki, ni)
+
+    # NB does not contain data terms, e.g. ln(x+1)
+    nll = weighted_nll_fast([alpha, beta], Aw, Bw, Nw)
+    exp_nll = 2876976.4476084057
+
+    np.testing.assert_allclose(nll, exp_nll, rtol=1e-5)
 
 
 def test_mle_fast(mock_bb_dataset, benchmark):
     (ki, ni), (exp_alpha, exp_beta) = mock_bb_dataset
     wi = np.ones_like(ki, dtype=float)
 
-    kmax = np.max(ki)
-    dkmax = np.max(ni - ki)
-    nmax = np.max(ni)
-
-    Aw = get_window_matrix(wi, ki, kmax)
-    Bw = get_window_matrix(wi, ni - ki, dkmax)
-    Nw = get_window_matrix(wi, ni, nmax)
+    Aw, Bw, Nw = get_window_matrices(wi, ki, ni)
 
     alpha = beta = 1.0
-    x0 = [alpha, beta]
+    x0 = np.array([alpha, beta])
 
-    """
     result = benchmark(
         minimize,
         weighted_nll_fast,
@@ -117,15 +165,8 @@ def test_mle_fast(mock_bb_dataset, benchmark):
         method="L-BFGS-B",
         bounds=[(1e-5, None), (1e-5, None)],
     )
-    """
 
-    result = minimize(
-        weighted_nll_fast,
-        x0,
-        args=(Aw, Bw, Nw),
-        method="L-BFGS-B",
-        bounds=[(1e-5, None), (1e-5, None)],
-    )
+    # print(result)
 
     assert result.success
 

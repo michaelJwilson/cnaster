bf520a69535bcdc6766471f6d821de0d8c377260
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-06-24 16:01:02 -0400

    fix

--- python/cnaster/hmrf.py
+++ python/cnaster/hmrf.py
@@ -22,7 +22,7 @@ from cnaster.hmm_initialize import plot_cna_mixture, cna_mixture_init
 from cnaster.pseudobulk import merge_pseudobulk_by_index_mix
 from cnaster.config import get_global_config
 from cnaster.plotting import plot_clones_spatial
-from cnaster.plot_genomic import plot_clones_genomic, plot_clones_genomic_raw
+from cnaster.plot_genomic import plot_clones_genomic
 from cnaster.deprecated.hmrf import (
     aggr_hmrfmix_reassignment_concatenate as dep_aggr_hmrfmix_reassignment_concatenate,
 )
